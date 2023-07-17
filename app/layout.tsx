b67b004f72ffc955671990e63e404c28f07ce1e3
Shaik Rehan <83860384+ShaikRehan123@users.noreply.github.com>
2023-07-17 10:00:41 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Header from "@/components/custom/Header";
 import { ThemeProvider } from "@/components/custom/ThemeProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Developer Marketplace",
@@ -21,6 +22,7 @@ export default function RootLayout({
           <Header />
           <div className="container py-8">{children}</div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
